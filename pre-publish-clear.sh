7ede7a8af5a047de919e4255d50afb868cf97e4d
Karolis Rusenas <karolis.rusenas@gmail.com>
2026-02-04 12:58:20 +0000

    cleanup

--- pre-publish-clear.sh
+++ pre-publish-clear.sh
@@ -1,2 +1,3 @@
 rm -rf /teamspace/studios/this_studio/.openclaw/agents/main/sessions
 rm -rf /teamspace/studios/this_studio/.openclaw/agents/main/*
+rm /teamspace/studios/this_studio/.openclaw/openclaw.json
